7f37b0f4177de4e8ba2ea66a3cc158f8088415bd
Logan Shimmer <loganherr@gmail.com>
2025-10-02 15:14:58 -0500

    Add admin dashboard, settings page, toaster, images, and portal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Toaster from '@/components/Toaster'
 
@@ -30,6 +31,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PQJWH536N9"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PQJWH536N9');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
